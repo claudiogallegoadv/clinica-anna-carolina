48184172d7bdf0165d4b8c3b94f4a49693c05c5b
claudiogallegoadv <claudiogallegoadv@gmail.com>
2026-06-12 16:36:48 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+// config.js — BANCO DE PRODUÇÃO
+// ⚠️ NÃO alterar este arquivo
+const SB_URL = 'https://nathaeuqbeqlvkftbmes.supabase.co';
+const SB_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5hdGhhZXVxYmVxbHZrZnRibWVzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk4OTc4MzYsImV4cCI6MjA5NTQ3MzgzNn0.9t3q5C_h1pRb11gqRtpGGVpOUGey5TBzvMgal8h6wtg';
